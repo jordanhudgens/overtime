f94b899119589f467e88b9b970ceef0d5d21dd8b
Sava Vidakovic <vidakovic.sava@gmail.com>
2016-08-30 16:56:19 +0200

    Update seed file, implement index view for posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,19 @@
+@user = User.create!(
+    email: 'test@foobar.com',
+    password: '123456',
+    password_confirmation: '123456',
+    first_name: 'John',
+    last_name: 'Snow'
+)
+
+puts 'User have been created'
+
 100.times do
-  Post.create!(date: Date.today, rationale: Faker::Lorem.sentence)
+  Post.create!(
+      date: Date.today,
+      rationale: Faker::Lorem.sentence,
+      user_id: @user.id
+  )
 end
 
 puts '100 Posts have been created'
