806ab9c07a13f73c94ce5143f1eddc6e3cffedf0
Sava Vidakovic <vidakovic.sava@gmail.com>
2016-08-30 17:23:06 +0200

    Add full_name to the user model, and implement to post partials

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,27 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'creation' do
-    before do
-      @user = User.create(
-          email: 'test@test.com',
-          password: '123456',
-          password_confirmation: '123456',
-          first_name: 'John',
-          last_name: 'Snow'
-      )
-    end
 
+  before do
+    @user = User.create(
+        email: 'test@test.com',
+        password: '123456',
+        password_confirmation: '123456',
+        first_name: 'John',
+        last_name: 'Snow'
+    )
+  end
+
+  describe 'creation' do
     it 'can be created' do
       expect(@user).to be_valid
     end
-
     it 'cannot be created without first_name, last_name' do
       @user.first_name = nil
       @user.last_name = nil
       expect(@user).to_not be_valid
     end
+  end
 
-
+  describe 'custom name methods' do
+    it 'has a full name method that combines first and last name' do
+      expect(@user.full_name).to eq('John, Snow')
+    end
   end
 end
